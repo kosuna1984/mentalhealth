f862002f055b09d0869b1c18dd247782a0a07a30
kosuna1984 <kosuna@mail.lipscomb.edu>
2018-04-04 22:00:42 -0500

    Add files via upload

--- mentalHealth.R
+++ mentalHealth.R
@@ -2,6 +2,9 @@
 
 #loading need libraries
 library(readr)
+library(stringr)
+library(tidyr)
+library(dplyr)
 
 
 #Uploading survey Mental health csv dataset file into r 
@@ -46,6 +49,68 @@ sum(is.na(mental_set$work_interfere))
 mental_set$work_interfere <- as.character(mental_set$work_interfere)
 mental_set$work_interfere[is.na(mental_set$work_interfere)] <- "Don't Know"
 mental_set$work_interfere <- as.factor(mental_set$work_interfere)
+
+##Cleaning "Gender" attribute  
+#Lower case all columm's elements in "Gender"
+levels(mental_set$Gender) <- tolower(levels(mental_set$Gender))
+
+#Select unique elements
+survey_gender <- unique(mental_set$Gender)
+View(survey_gender)
+
+
+#Making gender groups
+female_str <- c("cis female", "f", "female", "woman",  "femake", "female ","cis-female/femme", "female (cis)", "femail")
+male_str <- c("male", "m", "male-ish", "maile", "mal", "male (cis)", "make", "male ", "man","msle", "mail", "malr","cis man")
+trans_str <- c("trans-female", "something kinda male?", "queer/she/they", "non-binary","nah", "all", "enby", "fluid", "genderqueer", "androgyne", "agender", "male leaning androgynous", "guy (-ish) ^_^", "trans woman", "neuter", "female (trans)", "queer", "ostensibly male, unsure what that really means" )
+
+
+for(i in 1:nrow(mental_set))
+  {
+  for(k in 1:length(str_detect(mental_set$Gender[i],trans_str)))
+    if(str_detect(mental_set$Gender[i],trans_str)[k] == TRUE){
+      mental_set$Gender[i] <- "trans"
+    }
+  
+  for(m in 1:length(str_detect(mental_set$Gender[i],male_str)))
+    if(str_detect(mental_set$Gender[i],male_str)[m] == TRUE){
+      mental_set$Gender[i] <- "male"
+    }
+  
+  for(o in 1:length(str_detect(mental_set$Gender[i],female_str)))
+    if(str_detect(mental_set$Gender[i],female_str)[o] == TRUE){
+      mental_set$Gender[i] <- "female"
+    }
+}
+
+mental_set %<>% filter(Gender != "a little about you")
+mental_set %<>% filter(Gender != "guy (-ish) ^_^")
+mental_set %<>% filter(Gender != "p")
+
+
+
+
+
+
+
+
+
+
+NewGender <- replace(
+  male_str, 
+  mental_set$Gender, 
+  c("M")
+)
+
+
+
+
+
+
+
+
+
+
 
 
 
