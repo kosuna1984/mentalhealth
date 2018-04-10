248cc7c9e5cf16a4456a10fdb0fe7cac2d8769cc
kosuna1984 <kosuna@mail.lipscomb.edu>
2018-04-10 16:57:30 -0500

    Add files via upload

--- mentalHealth.R
+++ mentalHealth.R
@@ -1,4 +1,4 @@
-######################LIBRARY AND DATA LOADING
+###################### LIBRARY AND DATA LOADING
 
 # Function: installing and loading of packages
 install_load <- function (packages)  {   
@@ -31,23 +31,28 @@ install_load(libs.methods)
 mental_set <- read.csv("survey.csv")
 View(mental_set)
 
-#############################ANALYZING DATA
+############################# ANALYZING DATA
 #Whats the data row count? 
 dim(mental_set)
+#[1] 1259   27
 
 #whats the distribution of the data?
 head(mental_set)
+#Add this result to document
 
 #What types of data do we have?
 summary(mental_set)
+#Add this result to document
 
 
-############################DATA CLEANING
+############################ DATA CLEANING
 #Dealing with missing data
 #Let's get rid of the variables "Timestamp","comments", "state" since they don't provide a meaningful information for this investigation.
+#We should add some code to see this data why is not important for this investigation
 mental_set$Timestamp <- NULL
 mental_set$comments <- NULL
 mental_set$state <- NULL
+mental_set$Country <- NULL
 
 #Checking there's no missing data
 sum(is.na(mental_set)) #[1] 282
@@ -56,6 +61,7 @@ head(mental_set)
 sum(is.na(mental_set$self_employed)) 
 
 #Cleaning NaN in column "self_employed". There are only 0.014% of self employed so let's change NaN to NOT self_employed (No)
+#Calculating that 0.014 % to prove that
 mental_set$self_employed[is.na(mental_set$self_employed)] <- "No"
 #Double Checking
 sum(is.na(mental_set$self_employed))
@@ -66,7 +72,9 @@ apply(mental_set, 2, function(x) any(is.na(x)))
 #It looks like it is "work_interfere"
 sum(is.na(mental_set$work_interfere))
 
-#There are only 0.20% of self work_interfere so let's change NaN to Don't know (Don't Know)
+#Let's change NaN to Don't know (Don't Know) since this column refere to:
+#If you have a mental health condition, do you feel that it interferes with your work? 
+#If value is NaN, then we assume the individual left that value in blanck because he doesn't have a mental condition 
 mental_set$work_interfere <- as.character(mental_set$work_interfere)
 mental_set$work_interfere[is.na(mental_set$work_interfere)] <- "Don't Know"
 mental_set$work_interfere <- as.factor(mental_set$work_interfere)
@@ -109,52 +117,57 @@ mental_set <- mental_set %>% mutate(Age = replace(Age, Age > 65, 32))
 mental_set$Age[is.na(mental_set$Age)] <- 32
 summary(mental_set)
 
-###################################ENCODING DATA (FACTOR VARIABLES)
 #Checking what are factor variables
 str(mental_set)
 # $ Age                      : num  37 44 32 31 31 33 35 39 42 23 ...
 # $ Gender                   : chr  "female" "male" "male" "male" ...
 
-#Converting Gender into a factor
+#Converting Gender into a factor (character now) to convert dataset factor variables into numeric next
 mental_set$Gender <- as.factor(mental_set$Gender)
 
-#Saving dataset levels list
-mental_set_levels <- lapply(mental_set,levels)
 
-#Converting dataset factor variables into numeric
-mental_set[, 2:24] <- sapply(mental_set[, 2:24], as.numeric)
+################# VARIABILITY COMPARISON BETWEEN CATEGORIES OF VARIABLES
+#Effect size (strength of association)
+library(GoodmanKruskal)
+varset1<- c("Gender","self_employed","family_history","treatment","work_interfere","no_employees","remote_work","tech_company","benefits","care_options","wellness_program","seek_help","anonymity","leave","mental_health_consequence","phys_health_consequence","coworkers","supervisor","mental_health_interview","phys_health_interview","mental_vs_physical","obs_consequence")
+mentalFrame1<- subset(mental_set, select = varset1)
+GKmatrix1<- GKtauDataframe(mentalFrame1)
+plot(GKmatrix1, colors = "blue")
 
-
-
-#################COVARIANCE MATRIX. VARIABILITY COMPARISON BETWEEN CATEGORIES OF VARIABLES
-#correlation matrix
-corrmat <- cor(mental_set)
-corrplot(corrmat, method="color")
-
+### Importance variables: 
+#treatment(target), work_interfere, family_history, care_options, benefits, obs_consequence, anonymity, mental_health_interview, wellness_program, seek_help
 
 
-#"Treatment" variable correlation matrix
+########################### SOME CHARTS TO SEE DATA RELATIONSHIP
+# a <- ggplot(mental_set, aes(x = Age))
+# a + geom_histogram(aes(y = ..density..), 
+#                    colour="black", fill="white") +
+#   geom_density(alpha = 0.2, fill = "#FF6666")
+# 
+# 
+# 
+# #Initial data visualization
+# #Is there a relationship between treatment (target variable) and gender?
+# ggplot(mental_set, aes(Gender, ..count..)) + geom_bar(aes(fill = treatment), position = "dodge")
+# #We can notice from this graph that we have a higher number of males, and from males there have been more males not treated than treated
+# 
+# #Histogram with density plot by Age
+# a <- ggplot(mental_set, aes(x = Age))
+# a + geom_histogram(aes(y = ..density..), 
+#                    colour="black", fill="white") +
+#   geom_density(alpha = 0.2, fill = "#FF6666")
+# 
+# #Distribution and Density by Age Separate by treatment or not
+# # # Color by groups
+# # a + geom_histogram(aes(y = ..density.., color = treatment), 
+# #                    fill = "white",
+# #                    position = "identity")+
+# #   geom_density(aes(color = treatment), size = 1) +
+# #   scale_color_manual(values = c("#868686FF", "#EFC000FF"))
 
 
 
-###########################SOME CHARTS TO SEE DATA RELATIONSHIP
-#Histogram with density plot by Age
-a <- ggplot(mental_set, aes(x = Age))
-a + geom_histogram(aes(y = ..density..), 
-                   colour="black", fill="white") +
-  geom_density(alpha = 0.2, fill = "#FF6666")
-
-#Distribution and Density by Age Separate by treatment or not
-# # Color by groups
-# a + geom_histogram(aes(y = ..density.., color = treatment), 
-#                    fill = "white",
-#                    position = "identity")+
-#   geom_density(aes(color = treatment), size = 1) +
-#   scale_color_manual(values = c("#868686FF", "#EFC000FF"))
-
-
-
-###########################SCALING AND FITTING 
+########################### SCALING AND SPLITTING 
 #Normalizing or scaling "Age" variable, because is extremely different from the other ones (Min-Max Normalization)
 #(X - min(X))/(max(X) - min(X))
 #Above could be programmed as the following function in R:
@@ -165,13 +178,39 @@ normalize <- function(x) {
 mental_set$Age <- as.data.frame(lapply(mental_set["Age"], normalize))
 head(mental_set)
 
-#Spliltting the dataset
+#Saving dataset levels list for future reference
+mental_set_levels <- lapply(mental_set,levels)
 
+#Converting dataset factor variables into numeric
+mental_set[, 2:23] <- sapply(mental_set[, 2:23], as.numeric)
 
+# Selection of variables with higher variability
+lastMental<- data.frame(gender= mental_set$Gender,
+                   family_history= mental_set$family_history,
+                   work_interfere= mental_set$work_interfere,
+                   benefits= mental_set$benefits, 
+                   care_options= mental_set$care_options,
+                   anonymity= mental_set$anonymity,
+                   treatment=mental_set$treatment)
 
+# Preparing regression function for the use in other methods
+regresion <- treatment~
+  gender+
+  family_history+
+  work_interfere+
+  benefits+
+  care_options+
+  anonymity
 
+# Saving prediction percentage of each method
+percent <- data.frame(methods=c("Trees Classifier", "Neuronal Network","Randon Forest","Bagging"), value=c(0,0,0,0))
 
-
+# Data training and testing
+set.seed(101)
+n <- nrow(mental_set)
+data.index <- sample(1:n , size=round(n*0.7))
+train <- mental_set[data.index,]
+test <- mental_set[-data.index,]
 
 
 
